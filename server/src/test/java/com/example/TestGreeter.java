aed70a0a81e5d9f0cb3cd4832e07926aa3b36789
Ron Roche <ron.roche@workday.com>
2018-02-21 17:10:36 -0800

    Fix test

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "StarWars";
+    String someone = "World";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
